6ef87a4686a08a235de4a01687f9b862167fb1cc
Ilya Kos <mr.ilyakos@gmail.com>
2021-09-27 22:07:05 +0300

    Boots example

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import my_app
+from flask import render_template
 
 @my_app.route('/')
 @my_app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("main.html")
